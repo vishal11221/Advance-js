4eb17b8ca1ca7b1e66b4518cd51de34e87919c69
Vishal Raj <itsvishalraj11@gmail.com>
2023-12-13 11:25:41 +0530

    Advance js tutorial

---
+++ L-12(Modules)/library.js
@@ -0,0 +1,14 @@
+export {message,user,test};
+
+let message = "ES6 modules";
+
+function user(name){
+    console.log(`Hello ${name}`);
+}
+
+class test{
+    constructor(){
+        console.log("Constructor Method:--");
+    }
+}
+
---
+++ L-12(Modules)/main.js
@@ -0,0 +1,12 @@
+import {message,user,test} from "./library.js";
+// import * as Vishal from "./library.js";
+
+// console.log(Vishal.message);
+console.log(message);
+// document.body.innerHTML = message;
+
+user("Vishal Raj");
+
+// let a = new Vishal.test();
+let a = new test();
+
